44b64d7e2c74e0eb83a7d6ab1e24ed85d72a3b51
JAEHO SONG <thruthesky@gmail.com>
2018-10-17 20:08:43 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import Chance from 'chance';
+import { AngularWordpressApiService, UserCreate, UserResponse, PostCreate } from './angular-wordpress-api/angular-wordpress-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wordpress';
+
+  // user: UserResponse;
+  form: UserCreate = <any>{};
+  post: PostCreate = <any>{};
+  chance;
+  posts;
+  constructor(public wp: AngularWordpressApiService) {
+    if (this.wp.isLogged) {
+      this.wp.profile().subscribe(res => {
+        console.log('user profile: ', res);
+        this.form = <any>res;
+      }, e => alert(e.message));
+    }
+  }
+
+
+  onSubmitRegister() {
+    if (this.wp.isLogged) {
+      const data = {
+        email: this.form.email,
+        nickname: this.form.nickname
+      };
+      this.wp.updateProfile(data).subscribe(res => {
+        console.log('chane user:', res);
+      });
+
+    } else {
+      this.wp.register(this.form).subscribe(res => {
+        alert('Register success!');
+      });
+    }
+  }
+
+  onSubmitPostCreate() {
+    this.post.status = 'publish';
+    this.wp.postCreate(this.post).subscribe(res => {
+      console.log('create post: ', res);
+    }, e => alert(e.message));
+    return false;
+  }
 }
